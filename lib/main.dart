a04f88c1c571c82ad3002e073cb88d6c86365a62
unknown <sopu55544@gmail.com>
2022-11-16 23:48:48 +0900

    ㅇㅇ

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:date_test/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'sunflower',
+        textTheme: TextTheme(
+          headline1: TextStyle(
+            color: Colors.white,
+            fontFamily: 'parisienne',
+            fontSize: 80.0,
+          ),
+          headline2: TextStyle(
+            color: Colors.white,
+            fontSize: 50.0,
+            fontWeight: FontWeight.w700,
+          ),
+          bodyText1: TextStyle(
+            color: Colors.white,
+            fontSize: 30.0,
+          ),
+          bodyText2: TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+          ),
+        ),
+      ),
+      home: HomeScreen(),
+    ),
+  );
+}
